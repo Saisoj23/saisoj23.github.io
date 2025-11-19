a43613ba8afa18eae3a7a53ef000210da4ac6e3f
Saisoj23 <30736357+Saisoj23@users.noreply.github.com>
2025-11-19 12:28:18 -0300

    Try adding predeploy

--- Kanban-do/vite.config.ts
+++ Kanban-do/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://saisoj23.github.io/Kanban-do/",
+  base: "/saisoj23.github.io/Kanban-do",
   plugins: [tailwindcss(), react()],
 });
